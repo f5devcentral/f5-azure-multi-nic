f7ecea092d867dab97cc686b82c53ae6ecb103fe
Thomas Stanley <stanley@olympus.f5net.com>
2016-12-15 14:46:24 -0800

    updated cloud libs section

--- no_start.sh
+++ no_start.sh
@@ -32,27 +32,25 @@
 ## defaultGw (get this in this script)
 
 # Parse the command line arguments, primarily checking full params as short params are just placeholders
-while true; do
-    case "$1" in
-        -p|--adminpass)
-            adminpass=$2
-            shift 2;;
-        -h|--hostname)
-            hostname=$2
-            shift 2;;
-        -l|--location)
-            location=$2
-            shift 2;;
-        -k|--licenseKey)
-            licenseKey=$2
-            shift 2;;    
-        -y|--yesNo)
-            yesNo=$2
-            shift 2;;    
-        --)
-            shift
-            break;;
-    esac
+while getopts ":phlky" opt; do
+  case $opt in
+    p)
+      adminpass=$OPTARG
+      ;;
+    h)
+      hostname=$OPTARG
+      ;;
+    l)
+      location=$OPTARG
+      ;;
+    k)
+      licenseKey=$OPTARG
+      ;;
+    y)
+      yesNo=$OPTARG
+      set -x
+      ;;
+  esac
 done
 
 ## Get Private IP address of this device.
@@ -61,9 +59,5 @@ myip=$(ip route get 1 | awk '{print $NF;exit}')
 ## Get the Default Gateway IP address of this device.
 mydg=$(ip route get 1 | awk '{print $3;exit}')
 
-## Run and move the CloudLibs.
-f5-rest-node ./runScripts.js
-
 ## Execute the CloudLibs
-f5-rest-node ./config/f5-cloud-libs/scripts/azure/network.js --output /var/log/network.log --host ${hostname} -u admin -p ${adminpass} --multi-nic --default-gw ${mydg} --vlan vlan_mgmt,1.0 --self-ip self_mgmt,${myip},vlan_mgmt --log-level debug --background --force-reboot --signal NET_DONE
-f5-rest-node ./config/f5-cloud-libs/scripts/azure/onboard.js --wait-for NET_DONE --output /var/log/onboard.log --log-level debug --host ${myip} -u admin -p ${adminpass} --hostname ${hostname}.${location}.cloudapp.azure.com --set-password admin:${adminpass} --license ${licenseKey} --ntp pool.ntp.org --db tmm.maxremoteloglength:2048 --module ltm:nominal --signal ONBOARD_DONE
+exec f5-rest-node /config/f5-cloud-libs/scripts/azure/runScripts.js --log-level debug --tag v1.0.0 --onboard --output /var/log/onboard.log --log-level debug --host ${myip} -u admin -p ${adminpass}} --hostname ${hostname}.${location}.cloudapp.azure.com --set-password admin:${adminpass} --db tmm.maxremoteloglength:2048 --module ltm:nominal --signal ONBOARD_DONE --network --wait-for ONBOARD_DONE --output /var/log/network.log --host ${hostname} -u admin -p ${adminpass} --multi-nic --default-gw ${mydg} --vlan vlan_mgmt,1.0 --self-ip self_mgmt,${myip},vlan_mgmt --log-level debug --background --force-reboot --signal NET_DONE
