8c664b4fb32d965a731d1e89128c849b6cdd2ed7
Thomas Stanley <stanley@olympus.f5net.com>
2017-07-21 11:26:47 -0700

    updated cloudlibs and removed reboot

--- byol_start.sh
+++ byol_start.sh
@@ -75,7 +75,7 @@ master="${master}0"
 ## Execute the CloudLibs
 
 /usr/bin/f5-rest-node /config/cloud/f5-cloud-libs/scripts/onboard.js --output /var/log/onboard.log --log-level debug --host ${mgmtip} --port ${port} -u admin --password-url file:///config/cloud/passwd --hostname ${hostname}.${location}.cloudapp.azure.com --license ${licenseKey} --ntp pool.ntp.org --db tmm.maxremoteloglength:2048 --module ltm:nominal --no-reboot --signal ONBOARD_DONE &
-/usr/bin/f5-rest-node /config/cloud/f5-cloud-libs/scripts/network.js --wait-for ONBOARD_DONE --output /var/log/network.log --log-level debug --host ${mgmtip} --port ${port} -u admin --password-url file:///config/cloud/passwd --default-gw ${mydg} --vlan external,1.1 --vlan internal,1.2 --self-ip external_ip,${externalip},external --self-ip internal_ip,${udrip},internal --no-reboot --signal NETWORK_DONE &
+/usr/bin/f5-rest-node /config/cloud/f5-cloud-libs/scripts/network.js --wait-for ONBOARD_DONE --output /var/log/network.log --log-level debug --host ${mgmtip} --port ${port} -u admin --password-url file:///config/cloud/passwd --default-gw ${mydg} --vlan name:external,nic:1.1 --vlan name:internal,nic:1.2 --self-ip name:external_ip,address:${externalip},vlan:external --self-ip name:internal_ip,address:${internalip},vlan:internal --signal NETWORK_DONE &
 
 if [ ${cluster} == "Yes" ]; then
     if [ ${lastchar} == "0" ]; then
@@ -87,4 +87,3 @@ if [ ${cluster} == "Yes" ]; then
 fi
 wait
 rm -f /config/cloud/passwd
-reboot
